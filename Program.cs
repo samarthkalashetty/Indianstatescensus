52ccf5e26b1e5bd5acd5c3041ff13a0d7ed5826b
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-17 15:52:12 +0530

    Welcome to IndianStatesCensus Analyzer

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Indianstatescensus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyzer");
+        }
+    }
+}
