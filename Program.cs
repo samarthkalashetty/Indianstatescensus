4cd3abd4f5412a5392630ecf23d6bf86183d69c7
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-17 16:02:10 +0530

    Welcome ti UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Indianstatescensus
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class CSVStateCensus
 {
-    internal class Program
+    private string csvFilePath;
+
+    public CSVStateCensus(string filePath)
     {
-        static void Main(string[] args)
+        csvFilePath = filePath;
+    }
+
+    public IEnumerable<Dictionary<string, string>> LoadData()
+    {
+        using (StreamReader reader = new StreamReader(csvFilePath))
         {
-            Console.WriteLine("Welcome to Indian States Census Analyzer");
+            string[] headers = reader.ReadLine().Split(',');
+            while (!reader.EndOfStream)
+            {
+                string[] values = reader.ReadLine().Split(',');
+                Dictionary<string, string> dataEntry = new Dictionary<string, string>();
+
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dataEntry[headers[i]] = values[i];
+                }
+
+                yield return dataEntry;
+            }
         }
+    }
+}
+
+class StateCensusAnalyser
+{
+    private CSVStateCensus csvStateCensus;
+
+    public StateCensusAnalyser(string filePath)
+    {
+        csvStateCensus = new CSVStateCensus(filePath);
+    }
+
+    public IEnumerable<Dictionary<string, string>> Analyze()
+    {
+        IEnumerable<Dictionary<string, string>> data = csvStateCensus.LoadData();
+
+        // Perform analysis on the loaded data
+        // You can implement your analysis logic here
+
+        return data;  // Return the analyzed data
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string csvFilePath = "path_to_your_csv_file.csv"; // Replace with the actual path to your CSV file
+
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(csvFilePath);
+        IEnumerable<Dictionary<string, string>> analyzedData = stateCensusAnalyser.Analyze();
+
+        // Print or process the analyzed data as needed
     }
 }
